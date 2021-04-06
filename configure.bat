e2f6101fc2ae321eda1860dbde3f2db305c68115
DannyDeng <zxpay73945788@gmail.com>
2021-04-06 11:04:50 +0800

    update the configure.bat file for windows

--- configure.bat
+++ configure.bat
@@ -1,3 +1,3 @@
-echo Configure the makefile
-cmake -S . -B ./build -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=utils/arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
-echo ===================== completed =====================
+@echo Configure the makefile
+cmake -S . -B ./build -G"Ninja" -DCMAKE_TOOLCHAIN_FILE=utils/gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
+@echo ===================== completed =====================
