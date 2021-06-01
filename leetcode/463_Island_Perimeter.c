560d594b74794f869844d075f0b48adf1ecb5eef
Guillaume DannyDeng <zxpay73945788@gmail.com>
2021-06-01 20:44:26 +0800

    implement the island perimeter problem

--- leetcode/463_Island_Perimeter.c
+++ leetcode/463_Island_Perimeter.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int grid[4][4] = {
-    {0,1,0,0},
-    {1,1,1,0},
-    {0,1,0,0},
-    {1,1,0,0}
-};
-
-// int grid[4][3] = {
-//     {0,1,0},
-//     {1,1,0},
-//     {0,1,0},
-//     {1,1,0}
+// int grid[4][4] = {
+//     {0,1,0,0},
+//     {1,1,1,0},
+//     {0,1,0,0},
+//     {1,1,0,0}
 // };
+
+int grid[4][3] = {
+    {0,1,0},
+    {1,1,0},
+    {0,1,0},
+    {1,1,0}
+};
 
 void cpyGridToHeap(int **grid_dst, int *grid_src, int row, int col) {
     for(int i=0;i<row;i++) {
@@ -28,15 +28,48 @@ void cpyGridToHeap(int **grid_dst, int *grid_src, int row, int col) {
 }
 
 
-int islandPerimeter(int** grid, int gridSize, int* gridColSize) {
+int islandPerimeter(int **grid, int gridSize, int *gridColSize) {
+    int temp;
+    int cnt = 0;
     for(int i=0;i<gridSize;i++) {
         for(int j=0;j<*gridColSize;j++) {
-            
+            temp = (*grid)[j];
+            if(1 == temp) {
+                // check the top, left, right, down
+                // left
+                if(j-1 < 0) {
+                    cnt++;
+                }
+                else if((*grid)[j-1] == 0) {
+                    cnt++;
+                }
+                // right
+                if(j+1 == *gridColSize) {
+                    cnt++;
+                }
+                else if((*grid)[j+1] == 0) {
+                    cnt++;
+                }
+                // top
+                if(i-1 < 0) {
+                    cnt++;
+                }
+                else if ((*(grid-1))[j] == 0) {
+                    cnt++;
+                }
+                // down
+                if(i+1 == gridSize) {
+                    cnt++;
+                }
+                else if ((*(grid+1))[j] == 0) {
+                    cnt++;
+                }
+            }
         }
         grid++;
     }
     
-    return 0;
+    return cnt;
 }
 
 int main() {
@@ -47,13 +80,13 @@ int main() {
 
     int **arr = (int **)malloc(row * sizeof(int *));
     for (int i=0; i<4; i++)
-         arr[i] = (int *)malloc(col * sizeof(int));
+        arr[i] = (int *)malloc(col * sizeof(int));
 
     printf("copy the data to heap\n");
     cpyGridToHeap(arr, (int *)grid, row, col);
         
     printf("start algorithm\n");
-    printf("Perimeter: %d\n", islandPerimeter(arr, 4, &col));
+    printf("Perimeter: %d\n", islandPerimeter(arr, row, &col));
 
     return 0;
 }
