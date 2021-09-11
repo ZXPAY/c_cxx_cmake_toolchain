2a01a782726bf84e45029f2c8f69f7e28b1df175
Guillaume DannyDeng <zxpay73945788@gmail.com>
2021-09-11 08:58:34 +0800

    fix indent and disable build lib option

--- DannyLIB/src/packet_queue.c
+++ DannyLIB/src/packet_queue.c
@@ -5,46 +5,41 @@
 #include <assert.h>
 
 void init_packet_queue(packet_queue_t *packet_queue, uint8_t *buf_p, uint32_t packet_sz, uint32_t queue_len) {
-  packet_queue->enqueueIndex = 0;
-  packet_queue->dequeueIndex = 0;
-  packet_queue->packet_sz = packet_sz;
-  packet_queue->count = 0;
-  packet_queue->queue_len = queue_len;
-  packet_queue->buf_len = queue_len*packet_sz;
+    packet_queue->enqueueIndex = 0;
+    packet_queue->dequeueIndex = 0;
+    packet_queue->packet_sz = packet_sz;
+    packet_queue->count = 0;
+    packet_queue->queue_len = queue_len;
+    packet_queue->buf_len = queue_len*packet_sz;
 
-  packet_queue->data = buf_p;
-  assert(packet_queue->data != NULL);
-}
-
-void free_packet_queue(packet_queue_t *packet_queue) {
-  free(packet_queue->data);
-  memset(packet_queue, 0, sizeof(packet_queue_t));
+    packet_queue->data = buf_p;
+    assert(packet_queue->data != NULL);
 }
 
 void enqueue_packet(packet_queue_t *packet_queue, uint8_t *data) {
-  if(packet_queue->queue_len > packet_queue->count) {
-    memcpy(&packet_queue->data[packet_queue->enqueueIndex], data, packet_queue->packet_sz);
-    packet_queue->enqueueIndex += packet_queue->packet_sz;
-    if(packet_queue->enqueueIndex >= packet_queue->buf_len) {
-      packet_queue->enqueueIndex = 0;
+    if(packet_queue->queue_len > packet_queue->count) {
+        memcpy(&packet_queue->data[packet_queue->enqueueIndex], data, packet_queue->packet_sz);
+        packet_queue->enqueueIndex += packet_queue->packet_sz;
+        if(packet_queue->enqueueIndex >= packet_queue->buf_len) {
+            packet_queue->enqueueIndex = 0;
+        }
+        packet_queue->count++;
     }
-    packet_queue->count++;
-  }
-  else {
-    __asm("nop");
-  }
-  assert(packet_queue->queue_len >= packet_queue->count);
+    else {
+        assert(0);
+    }
+    assert(packet_queue->queue_len >= packet_queue->count);
 }
 
 void dequeue_packet(packet_queue_t *packet_queue) {
-  assert(packet_queue->count>0);
-  if(packet_queue->count>0) {
-    packet_queue->dequeueIndex += packet_queue->packet_sz;
-    if(packet_queue->dequeueIndex >= packet_queue->buf_len) {
-      packet_queue->dequeueIndex = 0;
+    assert(packet_queue->count>0);
+    if(packet_queue->count>0) {
+        packet_queue->dequeueIndex += packet_queue->packet_sz;
+        if(packet_queue->dequeueIndex >= packet_queue->buf_len) {
+            packet_queue->dequeueIndex = 0;
+        }
+        packet_queue->count--;
     }
-    packet_queue->count--;
-  }
 }
 
 
