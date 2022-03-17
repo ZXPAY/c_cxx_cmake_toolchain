c2e44789d31b3be29edee5f5a171cf2491e992c6
Guillaume DannyDeng <zxpay73945788@gmail.com>
2022-03-17 20:45:48 +0800

    practice leetcode 856

--- leetcode/856_Score_of_Parentheses.c
+++ leetcode/856_Score_of_Parentheses.c
@@ -6,34 +6,43 @@
 #include <assert.h>
 #include <math.h>
 
-// char test_string[] = "(((())))()()";
-char test_string[] = "(()(()))";
-
-
+// char test_string[] = "(((())))()()";  // 10
+char test_string[] = "(()(()))";      // 6
 
 int scoreOfParentheses(char *s) {
     int length = strlen(s);
+
+    int *buf = malloc(sizeof(int)*(length/2));
+    memset(buf, 0, sizeof(int)*(length/2));
+    int i_buf = 0;
     int count = 0;
-    int counts = 0;
+    
     for(int i=0;i<length;i++) {
         if(s[i] == '(') {
-            if(count == 0) count = 1;
-            else count *= 2;
-
-        }
-        else if(s[i] == ')') {
-            counts += count;
+            buf[i_buf++] = count;
             count = 0;
         }
+        else if(s[i] == ')') {
+            assert(i_buf>0);
+            if(count == 0) {
+                count = buf[i_buf-1] + 1;
+            }
+            else {
+                count = buf[i_buf-1] + 2 * count;
+            }
+            i_buf--;
+        }
+        // printf("= %d\n", count);
     }
 
-    return counts;
+    return count;
 }
 
 int main(int argc, char *argv[]) {
     // for(int i=0;i<argc;i++) {
     //     printf("arg[%d]: %s\n", i, argv[i]);
     // }
+
 
     printf("%d\n", scoreOfParentheses(test_string));
 
