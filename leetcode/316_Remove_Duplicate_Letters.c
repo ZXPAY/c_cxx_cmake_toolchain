eeb7804426a2af1aaff11c134edec013662b9c1a
Guillaume DannyDeng <zxpay73945788@gmail.com>
2022-03-20 16:26:09 +0800

    practice leetcode

--- leetcode/316_Remove_Duplicate_Letters.c
+++ leetcode/316_Remove_Duplicate_Letters.c
@@ -12,23 +12,52 @@ char test_string[] = "cbacdcbc"; // acdb
 
 char *removeDuplicateLetters(char *s) {
     int length = strlen(s);
-    int *char_count_map = malloc(26*sizeof(int));
-    memset(char_count_map, 0, 26*sizeof(int));
+    int *char_last_index = malloc(26*sizeof(int));
+    memset(char_last_index, 0, 26*sizeof(int));
+
+    bool *seen = malloc(26*sizeof(bool));
+    memset(seen, false, 26*sizeof(bool));
+
+    int *buf = malloc(length*sizeof(int));
+    memset(buf, 0, length*sizeof(int));    
+    int i_buf = 0;
 
     for(int i=0;i<length;i++) {
-        assert((s[i]-'a') < 26);
-        char_count_map[s[i]-'a']++;
+        char_last_index[s[i]-'a'] = i;
     }
-    int i_s = 0;
-    for(int i=0;i<length;i++) {
-        printf("[%c, %d]: %d\n", i+'a', i+'a', char_count_map[i]);
-        // if(char_count_map[i] != 0) s[i_s++] = (char)i+'a';
-        char_count_map[s[i]-'a']--;
 
-        // printf("%c, %d\n", s[i], char_count_map[s[i]-'a']);
+    for(int i=0;i<length;i++) {
+        int curr = s[i] - 'a';
+        if(seen[curr]) continue;
+        while(true) {
+            bool flag = false;
+            if(i_buf > 0) {
+                if(buf[i_buf-1] > s[i]) {
+                    if(i < char_last_index[buf[i_buf-1]-'a']) {
+                        flag = true;
+                    }
+                }
+            }
+            if(false == flag) break;
+            seen[buf[i_buf-1]-'a'] = false;
+            i_buf--;
+        }
+        buf[i_buf++] = s[i];
+        seen[curr] = true;
+
     }
-    printf("i_s: %d\n", i_s);
-    s[i_s] = '\0';
+
+    for(int i=0;i<i_buf;i++) {
+        s[i] = buf[i];
+    }
+
+    if(i_buf < length)
+        s[i_buf] = '\0';
+
+
+    free(char_last_index);
+    free(buf);
+    free(seen);
 
     return s;
 }
