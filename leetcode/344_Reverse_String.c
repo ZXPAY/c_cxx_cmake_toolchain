1304b827ecef23066923be48a20df98408402337
Danny Deng <dannydeng@biologue.net>
2022-04-01 09:53:58 +0800

    add a space

--- leetcode/344_Reverse_String.c
+++ leetcode/344_Reverse_String.c
@@ -14,7 +14,7 @@ void swap(char *a, char *b) {
     *b = temp;
 }
 
-void reverseString(char* s, int sSize){
+void reverseString(char* s, int sSize) {
     for(int i=0;i<sSize>>1;i++) {
         swap(&s[i], &s[sSize-i-1]);
     } 
