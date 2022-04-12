a7a6acd894fac3a283723c68159719188f8233ac
Danny Deng <dannydeng@biologue.net>
2022-04-12 16:11:39 +0800

    solve leetcode daily problem by using different way

--- leetcode/289_Game_of_Life.c
+++ leetcode/289_Game_of_Life.c
@@ -29,7 +29,6 @@ int test_board[4][3] = {
 #define M (sizeof(test_board) / sizeof(test_board[0]))
 #define N (sizeof(test_board) / M / sizeof(int))
 
-
 #define DEAD_CELL  0
 #define ALIVE_CELL 1
 int Conway_cell_logic(int **board, int r, int c, int m, int n) {
@@ -45,17 +44,20 @@ int Conway_cell_logic(int **board, int r, int c, int m, int n) {
     if(r-1 >= 0 && c-1 >= 0) eight_count += board[r-1][c-1];
     
     if(eight_count < 2) {
+        // 1 -> 0
         if(board[r][c] == ALIVE_CELL) {
             result = DEAD_CELL;
         }
     }
     else if(eight_count == 3) {
+        // 0 -> 1
         if(board[r][c] == DEAD_CELL) {
             // Alive the dead cell
             result = ALIVE_CELL;
         }
     }
     else if(eight_count > 3) {
+        // x -> 0
         result = DEAD_CELL;
     }
 
@@ -79,6 +81,77 @@ void gameOfLife(int **board, int boardSize, int *boardColSize) {
     free(board_copy);
 }
 
+/* Solution 2, Space complexity is O(1) */
+#define CASE_ONE_TO_ZERO    2    // previous is one
+#define CASE_ZERO_TO_ONE    3    // previous is zero
+const int eight_dir[8][2] = {
+    {-1,-1},
+    {-1, 0},
+    {-1, 1},
+    { 0, 1},
+    { 1, 1},
+    { 1, 0},
+    { 1,-1},
+    { 0,-1},
+};
+int Conway_cell_logic2(int **board, int r, int c, int m, int n) {
+    int eight_count = 0;
+    for(int i=0;i<8;i++) {
+        int nr = r + eight_dir[i][0];
+        int nc = c + eight_dir[i][1];
+        if(nr < 0 || nc < 0 || nr > (m-1) || nc > (n-1)) continue;
+
+        if(board[nr][nc] == CASE_ONE_TO_ZERO || board[nr][nc] == ALIVE_CELL) {
+            eight_count++;
+        }
+    }
+    
+    int result = board[r][c];
+    if(eight_count < 2) {
+        // 1 -> 0
+        if(board[r][c] == ALIVE_CELL) {
+            result = CASE_ONE_TO_ZERO;
+        }
+    }
+    else if(eight_count == 3) {
+        // 0 -> 1
+        if(board[r][c] == DEAD_CELL) {
+            // Alive the dead cell
+            result = CASE_ZERO_TO_ONE;
+        }
+    }
+    else if(eight_count > 3) {
+        // 1 -> 0
+        if(board[r][c] == ALIVE_CELL) {
+            result = CASE_ONE_TO_ZERO;
+        }
+    }
+
+    return result;
+}
+
+void gameOfLife2(int **board, int boardSize, int *boardColSize) {
+    // Copy the board
+    for(int i=0;i<boardSize;i++) {
+        for(int j=0;j<*boardColSize;j++) {
+            // iterate the board[i][j]
+            board[i][j] = Conway_cell_logic2(board, i, j, boardSize, *boardColSize);
+        }
+    }
+
+    // update the board[i][j] to 0 or 1
+    for(int i=0;i<boardSize;i++) {
+        for(int j=0;j<*boardColSize;j++) {
+            if(CASE_ONE_TO_ZERO == board[i][j]) {
+                board[i][j] = 0;
+            }
+            else if(CASE_ZERO_TO_ONE == board[i][j]) {
+                board[i][j] = 1;
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     // for(int i=0;i<argc;i++) {
     //     printf("arg[%d]: %s\n", i, argv[i]);
@@ -99,7 +172,8 @@ int main(int argc, char *argv[]) {
         printf("\n");
     }
     
-    gameOfLife(board, M, &col_size);
+    // gameOfLife(board, M, &col_size);
+    gameOfLife2(board, M, &col_size);
 
     printf("===== After =====\n");
     for(int i=0;i<M;i++) {
